84297855c0343ec15637a5f1a4a208ac143f1378
Remorse <thesirremorse@gmail.com>
2025-02-05 23:49:29 +0000

    Convert to slash commands
    Converted remaining utility commands to slash commands.
    Edited the way embeds are displayed
    Modified ping embed to reflect a colour based on the ping at levels 0-100 100-200 and 200+

--- cogs/utility.py
+++ cogs/utility.py
@@ -5,39 +5,42 @@ import discord
 from discord.ext import commands
 import discord.app_commands
 
+
 class Utility(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    # TODO: Convert this to a slash command
-    @commands.command(name="ping")
-    @commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
-    async def ping(self, ctx):
-        embed = discord.Embed(title="Ping", colour=discord.Colour.blue())
-        embed.add_field(name=f"{self.bot.user.name}'s Latency (ms): ", value=f"{round(self.bot.latency * 1000)}ms", inline=True)
-        embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar)
-        embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar)
-        await ctx.send(embed=embed)
 
-    # TODO: Convert this to a slash command
-    @commands.command(name="serverinfo")
-    @commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
-    async def server_info(self, ctx):
-        embed = discord.Embed(title="Server Information", colour=discord.Colour.blue())
-        embed.add_field(name="Server name", value=f"{ctx.guild.name}", inline=True)
-        embed.add_field(name="Server ID", value=f"{ctx.guild.id}", inline=True)
-        embed.add_field(name="Owner", value=f"{ctx.guild.owner}", inline=True)
-        embed.add_field(name="User count", value=f"{ctx.guild.member_count}", inline=True)
-        embed.add_field(name="Text channel count", value=f"{len(ctx.guild.channels)}", inline=True)
-        embed.add_field(name="Voice channel count", value=f"{len(ctx.guild.voice_channels)}", inline=True)
-        embed.add_field(name="Emojis", value=f"{len(ctx.guild.emojis)}", inline=True)
-        embed.add_field(name="Stickers", value=f"{len(ctx.guild.stickers)}", inline=True)
-        embed.add_field(name="Roles", value=f"{len(ctx.guild.roles)}", inline=True)
-        embed.add_field(name="", value=f"", inline=False)
-        embed.add_field(name="Created at", value=f"{ctx.guild.created_at}", inline=True)
-        embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar)
-        embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar)
-        embed.set_thumbnail(url=ctx.guild.icon)
-        await ctx.send(embed=embed)
+    @discord.app_commands.command(name="ping", description="Display the bots current latency (ms).")
+    @discord.app_commands.checks.cooldown(rate=1, per=10)
+    async def ping(self, interaction: discord.Interaction):
+        latency = round(self.bot.latency * 1000)
+
+        colour = (
+            discord.Colour.green() if latency < 100 else
+            discord.Colour.yellow() if latency < 200 else
+            discord.Colour.red()
+        )
+
+        embed = discord.Embed(description=f"{latency}ms", colour=colour)
+        await interaction.response.send_message(embed=embed)
+
+    @discord.app_commands.command(name="serverinfo", description="Provide information on the current server.")
+    @discord.app_commands.checks.cooldown(rate=1, per=10)
+    async def server_info(self, interaction: discord.Interaction):
+        created_at = interaction.guild.created_at
+        formatted_time = created_at.astimezone(datetime.timezone.utc).strftime("%B %d, %Y at %I:%M %p UTC")
+        embed = discord.Embed(title="Server Information", colour=discord.Colour.green())
+        embed.add_field(name="Server name", value=f"{interaction.guild.name}", inline=True)
+        embed.add_field(name="Server ID", value=f"{interaction.guild.id}", inline=True)
+        embed.add_field(name="Owner", value=f"{interaction.guild.owner}", inline=True)
+        embed.add_field(name="User count", value=f"{interaction.guild.member_count}", inline=True)
+        embed.add_field(name="Text channel count", value=f"{len(interaction.guild.channels)}", inline=True)
+        embed.add_field(name="Voice channel count", value=f"{len(interaction.guild.voice_channels)}", inline=True)
+        embed.add_field(name="Roles", value=f"{len(interaction.guild.roles)}", inline=True)
+        embed.add_field(name="", value="", inline=False)
+        embed.add_field(name="Created at", value=f"{formatted_time}", inline=True)
+        embed.set_thumbnail(url=interaction.guild.icon)
+        await interaction.response.send_message(embed=embed)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -45,20 +48,13 @@ class Utility(commands.Cog):
         print("Initialising uptime timer")
         startTime = time.time()
 
-    @discord.app_commands.command(name="uptime", description="Display the bots current uptime")
+    @discord.app_commands.command(name="uptime", description="Display the bots current uptime.")
+    @discord.app_commands.checks.cooldown(rate=1, per=10)
     async def uptime(self, interaction: discord.Interaction):
         uptime = str(datetime.timedelta(seconds=int(round(time.time() - startTime))))
-        embed = discord.Embed(title="Uptime", description=f"{self.bot.user.name} has been running for `{uptime}`", colour=discord.Colour.blue())
-        embed.set_footer(text=f"Requested by {interaction.user.name}", icon_url=interaction.user.avatar)
-        embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar)
+        embed = discord.Embed(description=f"{self.bot.user.name} has been running for `{uptime}`",
+                              colour=discord.Colour.green())
         await interaction.response.send_message(embed=embed)
-
-    @discord.app_commands.command(name="test", description="A test slash command")
-    async def test(self, interaction: discord.Interaction):
-        await interaction.response.send_message("This was a test slash command")
-
-
-
 
 async def setup(bot):
     await bot.add_cog(Utility(bot))
